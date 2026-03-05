6c34556092d015f636c6988cc9ae2fa92a67b4fe
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-05 14:40:50 +0300

    git init and base project

---
+++ src/main/java/com/evgeny/innowisetasks/InnowiseTasksApplication.java
@@ -0,0 +1,13 @@
+package com.evgeny.innowisetasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evgeny/innowisetasks/InnowiseTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evgeny.innowisetasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
