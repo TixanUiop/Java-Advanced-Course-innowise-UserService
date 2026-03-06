e1f410399d6a8305ba7984896df098e0916877b4
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-06 13:18:45 +0300

    Added BaseEntity class for audit fields and finished creating main entities

--- src/main/java/com/evgeny/innowisetasks/Entity/UserEntity.java
+++ src/main/java/com/evgeny/innowisetasks/Entity/UserEntity.java
@@ -1,4 +1,43 @@
 package com.evgeny.innowisetasks.Entity;
 
-public class UserEntity {
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.smartcardio.Card;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private Boolean active = true;
+
+    @OneToMany(
+            mappedBy = "users",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<PaymentCardsEntity> cards;
 }
