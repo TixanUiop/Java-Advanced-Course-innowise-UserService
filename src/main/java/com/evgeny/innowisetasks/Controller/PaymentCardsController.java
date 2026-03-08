14fa37b7da7b0dd11f31b987797afa2052143084
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-08 15:44:10 +0300

    added base controllers

--- src/main/java/com/evgeny/innowisetasks/Controller/PaymentCardsController.java
+++ src/main/java/com/evgeny/innowisetasks/Controller/PaymentCardsController.java
@@ -1,6 +1,6 @@
 package com.evgeny.innowisetasks.Controller;
 
-
+import com.evgeny.innowisetasks.DTO.PaymentCardsDTO;
 import com.evgeny.innowisetasks.DTO.UserDTO;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
@@ -9,28 +9,27 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/users")
-public class UserController {
+@RequestMapping("/api/v1/payment-cards")
+public class PaymentCardsController {
 
     @GetMapping
-    public List<UserDTO> getAllUsers() {
+    public List<PaymentCardsDTO> getAllPaymentCards() {
         return null; //get all
     }
 
     @GetMapping("/{id}")
-    public Optional<UserDTO> getUserById(Long id)
+    public Optional<PaymentCardsDTO> getPaymentCardsById(Long id)
     {
         return Optional.empty(); //get user by id
     }
 
-    @PostMapping
-    public Optional<UserDTO> create(@Valid @RequestBody UserDTO dto) {
+    @PostMapping("/create")
+    public Optional<PaymentCardsDTO> create(@Valid @RequestBody PaymentCardsDTO dto) {
         return null; //create user
     }
 
-    @PutMapping("/update/{id}")
-    public UserDTO update(
-            @PathVariable Long id,
+    @PutMapping("/update")
+    public PaymentCardsDTO update(
             @Valid @RequestBody UserDTO dto) {
 
         return null; //update user
@@ -38,7 +37,7 @@ public class UserController {
 
     @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable Long id) {
-         //delete user
+        //delete user
     }
 
 }
