3bdb99cf048ef4230ebc555cc6e289a841cc9974
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-14 18:05:12 +0300

    add unit tests for service layer

---
+++ src/test/java/com/evgeny/innowisetasks/Service/PaymentCardServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.evgeny.innowisetasks.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentCardServiceImplTest {
+
+    @Test
+    void create() {
+    }
+}
