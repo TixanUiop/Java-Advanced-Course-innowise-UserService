c7b51179e2899b94c10c433a7f0c7c0c37cccf12
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-16 15:10:02 +0300

    added EntityScan

--- src/main/java/com/evgeny/innowisetasks/InnowiseTasksApplication.java
+++ src/main/java/com/evgeny/innowisetasks/InnowiseTasksApplication.java
@@ -3,16 +3,18 @@ package com.evgeny.innowisetasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadataProvidersConfiguration.class
-})
+
 @EnableJpaAuditing
 @EnableCaching
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.evgeny.innowisetasks.Repository")
+@EntityScan(basePackages = "com.evgeny.innowisetasks.Entity")
 public class InnowiseTasksApplication {
 
     public static void main(String[] args) {
